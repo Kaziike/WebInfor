9f39e88e4473ec843023585ba208eed2dd80798a
Chau <mchaukaz@gmail.com>
2026-04-29 10:17:26 +0700

    Buoi5

---
+++ js/lib7.js
@@ -0,0 +1,20 @@
+const productlist = [
+    {
+        name: "T90",
+        description: "T90 là một chiếc xe tăng chiến đấu chủ lực hiện đại nhất của Nga, được thiết kế để đối phó với các mối đe dọa trên chiến trường hiện đại.", 
+        price: 5000000000,
+        img:"../assets/images/t90.jpg",
+    },
+    {
+        name: "M1 Abrams",
+        description: "M1 Abrams là một chiếc xe tăng chiến đấu chủ lực được sử dụng bởi Quân đội Hoa Kỳ, nổi tiếng với khả năng bảo vệ và sức mạnh hỏa lực vượt trội.", 
+        price: 6000000000,
+        img:"../assets/images/m1.jpg",
+    },
+    {
+        name: "Leopard 2",
+        description: "Leopard 2 là một chiếc xe tăng chiến đấu chủ lực được sản xuất bởi Đức, được biết đến với khả năng cơ động và sức mạnh hỏa lực cao.", 
+        price: 5500000000,
+        img:"../assets/images/leopard2.jpg",
+    }
+];
